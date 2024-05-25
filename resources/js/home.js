7142214ae9fbc99faf8f696ab7b384acbf99b2ca
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2024-05-25 19:38:52 +0330

    master

--- resources/js/home.js
+++ resources/js/home.js
@@ -2,36 +2,5 @@ window.$ =
     window.jQuery =
     window.jquery =
         require("../assets/home/js/vendor/jquery-3.2.1.min.js");
-require("../assets/home/js/vendor/owl.carousel.min.js");
-require("../assets/home/js/vendor/jquery.countdown.js");
-require("../assets/home/js/vendor/ResizeSensor.min.js");
-require("../assets/home/js/vendor/theia-sticky-sidebar.min.js");
-window.wNumb = require("../assets/home/js/vendor/wNumb.js");
-window.noUiSlider = require("../assets/home/js/vendor/nouislider.min.js");
-
-require("../assets/home/js/vendor/jquery.nice-select.min.js");
-require("../assets/home/js/vendor/bootstrap-slider.min.js");
-
-require("../assets/home/js/vendor/jquery.jqZoom.js");
 window.Swal =
     window.swal = require("../assets/home/js/vendor/sweetalert2.all.min.js");
-require("../assets/home/js/vendor/cloudflar.js");
-require("../assets/admin/js/rating.js");
-
-// require("../assets/home/js/vendor/bootstrap.bundle.js");
-require("../assets/home/js/vendor/lightgallery.js");
-require("../assets/home/js/vendor/jquery.ez-plus.js");
-require("../assets/home/js/main.js");
-
-//import Echo from "laravel-echo";
-
-//window.Pusher = require("pusher-js");
-
-//window.Echo = new Echo({
-//    broadcaster: "pusher",
-//    key: process.env.MIX_PUSHER_APP_KEY,
-//    wsHost: window.location.hostname,
-//    wsPort: 6001,
-//    forceTLS: false,
-//   disableStats: true,
-//});
--- resources/views/home/layout/MasterHome.blade.php
+++ resources/views/home/layout/MasterHome.blade.php
@@ -1,84 +1,16 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
-
 <head>
     @include('home.partial.Head')
     @stack('styles')
     @livewireStyles()
-    <style>
-        .form1 {
-            position: relative;
-        }
-
-        .form1 i {
-
-            position: absolute;
-            top: 4px;
-            left: 10px;
-            padding: 7px;
-            color: #9ca3af;
-            cursor: pointer;
-            font-size: 1.2em;
-
-        }
-
-        .form1 span {
-            position: absolute;
-            right: 0px;
-            top: 1px;
-            padding: 2px 5px;
-            border-left: 1px solid #d1d5db;
-        }
-
-        .form1 .left-pan1 select {
-            font-size: 14px;
-            text-align: center;
-        }
-
-        .form1 .left-pan1 select:focus {
-            border: none;
-            box-shadow: none;
-        }
-
-        .form-input {
-            height: 44px;
-            text-indent: 110px;
-            border-radius: 10px;
-            font-size: 14px;
-        }
-
-        .form-input:focus {
-            box-shadow: none;
-            border: none;
-        }
-    </style>
 </head>
-
 <body class="container-main-xlg mx-auto">
-{{--    @includeUnless(request()->routeIs('login', 'register'), 'home.partial.Header')--}}
-{{--    @includeUnless(request()->routeIs('login', 'register'), 'home.partial.Modal')--}}
-
     @yield('content')
-
-{{--    @includeUnless(request()->routeIs('login', 'register'), 'home.partial.Footer')--}}
-{{--    @include('home.partial.Scroll')--}}
-    {{-- @include('home.partial.Loader') --}}
-
-    <script src="{{ asset('assets/admin/jquery-3.2.1.min.js') }}"></script>
-
-
     <script type="text/javascript" src="{{ asset('js/home.js') }}"></script>
-
     @livewireScripts()
     @include('sweetalert::alert')
     @stack('scripts')
-
-    <!---start GOFTINO code--->
-    {{-- <script type="text/javascript">
-        !function(){var i="REbu0W",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
-    </script> --}}
-    <!---end GOFTINO code--->
-
     <script>
         @if (session('status'))
             @if (session('status') == 'profile-information-updated')
@@ -121,8 +53,5 @@
             @endif
         @endif
     </script>
-    <script type="text/javascript" src="{{ asset('assets/home/js/vendor/bootstrap.bundle.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/home/js/vendor/jquery.touchSwipe.min.js') }}"></script>
 </body>
-
 </html>
