47a36f27eba83b8239706deba2c41a00ad4e3698
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2024-05-27 00:22:34 +0330

    XCV

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -33,7 +33,7 @@ class DashboardController extends Controller
         //دستگاه ها ی بررسی نشده
         $status_device_checks = $devices->where('status', 0)->latest()->take(5);
 
-        $users = User::role('personnel')->latest()->take(5)->get();
+        $users = User::role('personnel')->sortBy('desc')->take(5)->get();
 
         $actions = Action::whereBetween('created_at', [$from, $to])->where('status', 1)->latest()->take(5)->get();
         $image = Guide::where('type', 'image')->where('category', 'banner')->latest()->first();
