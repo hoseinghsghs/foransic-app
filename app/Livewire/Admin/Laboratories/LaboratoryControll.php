610429addc4d6fa7d9d56b9cefec63a31188c7b8
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2024-05-26 21:17:31 +0330

    labratory add item

--- app/Livewire/Admin/Laboratories/LaboratoryControll.php
+++ app/Livewire/Admin/Laboratories/LaboratoryControll.php
@@ -9,6 +9,19 @@ class LaboratoryControll extends Component
 {
     protected $paginationTheme = 'bootstrap';
     public $name;
+    public $province = "";
+    public $place = "";
+    public $internal_number = "";
+    public $permanent_personnel_count = "";
+    public $temporary_personnel_count = "";
+    public $laptop_count = "";
+    public $tablet_count = "";
+    public $version_ufed_for_pc = "";
+    public $version_ufed_analyzer = "";
+    public $version_oxygen = "";
+    public $version_axiom = "";
+    public $version_final_mobile = "";
+    public $description = "";
     public Laboratory $laboratory;
     public $is_edit = false;
     public $display;
@@ -31,12 +44,37 @@ class LaboratoryControll extends Component
                 'laboratory.id' => 'required|exists:laboratories,id',
             ]);
 
-            $this->laboratory->update([
-                'name' => $this->name,
+            $this->laboratory->update(["name" => $this->name,
+                "province" => $this->province,
+                "place" => $this->place,
+                "internal_number" => $this->internal_number,
+                "permanent_personnel_count" => $this->permanent_personnel_count,
+                "temporary_personnel_count" => $this->temporary_personnel_count,
+                "laptop_count" => $this->laptop_count,
+                "tablet_count" => $this->tablet_count,
+                "version_ufed_for_pc" => $this->version_ufed_for_pc,
+                "version_ufed_analyzer" => $this->version_ufed_analyzer,
+                "version_oxygen" => $this->version_oxygen,
+                "version_axiom" => $this->version_axiom,
+                "version_final_mobile" => $this->version_final_mobile,
+                "description" => $this->description,
             ]);
 
             $this->is_edit = false;
             $this->reset("name");
+            $this->reset("province");
+            $this->reset("place");
+            $this->reset("internal_number");
+            $this->reset("permanent_personnel_count");
+            $this->reset("temporary_personnel_count");
+            $this->reset("laptop_count");
+            $this->reset("tablet_count");
+            $this->reset("version_ufed_for_pc");
+            $this->reset("version_ufed_analyzer");
+            $this->reset("version_oxygen");
+            $this->reset("version_axiom");
+            $this->reset("version_final_mobile");
+            $this->reset("description");
             $this->reset("display");
             toastr()->rtl()->addSuccess('تغییرات با موفقیت ذخیره شد', ' ');
         } else {
@@ -47,6 +85,19 @@ class LaboratoryControll extends Component
             ]);
             Laboratory::create([
                 "name" => $this->name,
+                "province" => $this->province,
+                "place" => $this->place,
+                "internal_number" => $this->internal_number,
+                "permanent_personnel_count" => $this->permanent_personnel_count,
+                "temporary_personnel_count" => $this->temporary_personnel_count,
+                "laptop_count" => $this->laptop_count,
+                "tablet_count" => $this->tablet_count,
+                "version_ufed_for_pc" => $this->version_ufed_for_pc,
+                "version_ufed_analyzer" => $this->version_ufed_analyzer,
+                "version_oxygen" => $this->version_oxygen,
+                "version_axiom" => $this->version_axiom,
+                "version_final_mobile" => $this->version_final_mobile,
+                "description" => $this->description,
             ]);
             $this->reset("name");
             toastr()->rtl()->addSuccess('آزمایشگاه با موفقیت ایجاد شد', ' ');
@@ -59,6 +110,19 @@ class LaboratoryControll extends Component
 
         $this->is_edit = true;
         $this->name = $laboratory->name;
+        $this->province = $laboratory->province;
+        $this->place = $laboratory->place;
+        $this->internal_number = $laboratory->internal_number;
+        $this->permanent_personnel_count = $laboratory->permanent_personnel_count;
+        $this->temporary_personnel_count = $laboratory->temporary_personnel_count;
+        $this->laptop_count = $laboratory->laptop_count;
+        $this->tablet_count = $laboratory->tablet_count;
+        $this->version_ufed_for_pc = $laboratory->version_ufed_for_pc;
+        $this->version_ufed_analyzer = $laboratory->version_ufed_analyzer;
+        $this->version_oxygen = $laboratory->version_oxygen;
+        $this->version_axiom = $laboratory->version_axiom;
+        $this->version_final_mobile = $laboratory->version_final_mobile;
+        $this->description = $laboratory->description;
         $this->laboratory = $laboratory;
         $this->display = "disabled";
     }
