5af20eed49cd09530255edbe00591149e06cff3c
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2024-06-08 20:53:09 +0330

    print

--- resources/views/admin/page/prints/printdevice.blade.php
+++ resources/views/admin/page/prints/printdevice.blade.php
@@ -6,8 +6,19 @@
      @include('admin.partial.Head')
      <link rel="stylesheet" href="{{ mix('css/admin.css') }}">
      <style>
+         * {
+             margin: 0;
+             padding: 0;
+             border: 0;
+             outline: 0;
+             font-size: 100%;
+             vertical-align: baseline;
+             background: transparent;
+         }
+
          .table-bordered thead td,
          .table-bordered thead th {
+             border: 3px solid #000000 !important;
              border-bottom-width: 2px !important;
          }
 
@@ -18,32 +29,36 @@
 
          .table-bordered td,
          .table-bordered th {
-             border: 1px solid #000000 !important;
+
+             border: 3px solid #000000 !important;
          }
 
          .table-bordered {
-             border: 1px solid #000000;
+             border: 3x solid #000000;
          }
      </style>
  </head>
 
- <body class="theme-blush" id="cheack_collapsed" style="width: 80% ; margin: auto;font-size: 1.5rem">
+ <body class="theme-blush" id="cheack_collapsed" style="width: 50% ; margin: auto;font-size: 1.5rem">
      <div class="container-xl mt-4" style="margin-top: 100px ">
          <div class="row mt-4">
-             <div class="col-3 text-center"><img src="{{ $setting->logo ? asset('storage/logo/' . $setting->logo) : '/images/logo.png' }}" alt="logo" height="45px" /></div>
+             <div class="col-3 text-center"><img src="{{ $setting->logo ? asset('storage/logo/' . $setting->logo) : '/images/logo.png' }}" alt="logo" height="130rem" width="260rem" /></div>
              <div class="col-6 text-center">
-                 <h5 class="font-weight-bold">رسید پذیرش آزمایشگاه جرم یابی دیجیتال</h5>
+                 <h5 class="font-weight-bold">رسید پذیرش آزمایشگاه
+                     </br>
+                     جرم یابی دیجیتال</h5>
              </div>
              <div class="col-3 text-right">
-                 <h6> کد یکتا: {{ $device->id }}</h6>
-                 <h6>تاریخ : {{ Hekmatinasser\Verta\Verta::instance($device->created_at)->format('Y/n/j') }}</h6>
+                 <h5 class="font-weight-bold">تاریخ : {{ Hekmatinasser\Verta\Verta::instance($device->created_at)->format('Y/n/j') }}</h6>
              </div>
          </div>
          <div class="row m-3">
              <table class="table table-bordered">
                  <thead>
                      <tr>
-                         <th class="text-center" colspan="11">مشخصات تحویل دهنده</th>
+                         <th class="text-center" colspan="11">
+                             <h5 class="font-weight-bold">مشخصات تحویل دهنده</h5>
+                         </th>
                      </tr>
                  </thead>
                  <tbody>
@@ -52,22 +67,26 @@
                              <div class="row">
 
                                  <div class="col-12">
-                                     <p> رده:
+                                     <p>
+                                     <h5 class="font-weight-bold">رده:
                                          @if($device->dossier()->exists())
                                          {{ $device->dossier->company->name }}
                                          @else
                                          ندارد
                                          @endif
+                                     </h5>
                                      </p>
                                  </div>
 
-                                 <div class="col-12">
-                                     <p>نام شخص حقیقی / حقوقی تحویل دهنده : {{ $device->delivery_name }} </p>
-
+                                 <div class="col-6">
+                                     <p>
+                                     <h5 class="font-weight-bold">نام : {{ $device->delivery_name }} </h5>
+                                     </p>
                                  </div>
-                                 <div class="col-12">
-                                     <p>کد پرسنلی : {{ $device->delivery_code }} </p>
-
+                                 <div class="col-6">
+                                     <p>
+                                     <h5 class="font-weight-bold">کد پرسنلی : {{ $device->delivery_code }} </h5>
+                                     </p>
                                  </div>
 
                              </div>
@@ -81,72 +100,110 @@
                  </tbody>
                  <thead>
                      <tr>
-                         <th class="text-center" colspan="11">مشخصات تحویل گیرنده</th>
+                         <th class="text-center" colspan="11">
+                             <h5 class="font-weight-bold"> مشخصات تحویل گیرنده </h5>
+                         </th>
                      </tr>
                  </thead>
                  <tbody>
                      <tr>
                          <td colspan="11" class="text-right">
                              <div class="row">
+
                                  <div class="col-12">
-                                     <p>پرسنل تحویل گیرنده :
-                                         {{ $receiver_staff_id->name }}
+                                     <p>
+                                     <h5 class="font-weight-bold">
+                                         آزمایشگاه :
+                                         {{ $device->laboratory->name }}
+                                     </h5>
                                      </p>
                                  </div>
-                                 {{-- <div class="col-4">
-                                     <p>کد پستی:{{ $device->delivery_code }}</p>
+
+
+                                 <div class="col-12">
+                                     <p>
+                                     <h5 class="font-weight-bold">
+                                         پرسنل تحویل گیرنده :
+                                         {{ $receiver_staff_id->name }}
+                                     </h5>
+                                     </p>
+                                 </div>
                              </div>
-                             <div class="col-4">
-                                 <p>تلفن / نمابر:{{ $device->delivery_name }} ,
-                                     {{ $device->receiver_staff_id }}
-                                 </p>
-                             </div> --}}
+                         </td>
+
+                     </tr>
+                 </tbody>
+                 <thead>
+                     <tr>
+                         <th class="text-center" colspan="11">
+                             <h5 class="font-weight-bold">
+                                 شواهد دیجیتال
+                             </h5>
+                         </th>
+                     </tr>
+                 </thead>
+                 <tr class="text-center">
+                     <th>
+                         <h5 class="font-weight-bold">عنوان شاهد</h5>
+                     </th>
+                     <th>
+                         <h5 class="font-weight-bold">مدل</h5>
+                     </th>
+                     <th>
+                         <h5 class="font-weight-bold">کد یکتا</h5>
+                     </th>
+
+                 </tr>
+
+                 <tbody>
+                     <tr class="text-center">
+                         <td>
+                             <h2 class="font-weight-bold"> {{ $device->category->title }}</h2>
+                         </td>
+                         <td>
+                             <h2 class="font-weight-bold"> {{ $device->code }}</h2>
+                         </td>
+                         <td>
+                             <h2 class="font-weight-bold"> {{ $device->id }}</h2>
+                         </td>
+
+                     </tr>
+
+                     <tr>
+                         <th colspan="6" class="text-right" style="padding-bottom:5rem ">
+                             <h5 class="font-weight-bold">لوازم جانبی:
+                                 {!! $device->accessories !!}
+                             </h5>
+                         </th>
+                     </tr>
+                     <tr>
+
+                         <th colspan="6" class="text-right">
+                             <h5 class="font-weight-bold">
+                                 مشخصات (ظرفیت ، مدل و...)
+                                 : {!! $device->trait !!}
+                             </h5>
+                         </th>
+                     </tr>
+                 </tbody>
+
+                 </tbody>
+             </table>
+
          </div>
-         </td>
-         </tr>
-         </tbody>
-         <thead>
-             <tr>
-                 <th class="text-center" colspan="11"> شواهد دیجیتال</th>
-             </tr>
-         </thead>
-         <tr class="text-center">
-             <th>id</th>
-             <th>مدل</th>
-             <th>عنوان کالا</th>
-             <th> مشخصات </th>
-         </tr>
+         <div class="row" dir="rtl" style="width: 100%;margin-bottom: 6rem ; margin-top: 2rem ; border-bottom: 3px dashed #000000 ;">
+             <div class="col-6" style="text-align: center; margin-bottom: 7rem;">
+                 <h5 class="font-weight-bold">
+                     مهر و امضا تحویل گیرنده
+                 </h5>
+             </div>
+             <div class="col-6" style="text-align: center; margin-bottom:7rem;">
+                 <h5 class="font-weight-bold">
+                     مهر و امضا تحویل دهنده
+                 </h5>
+             </div>
 
-         <tbody>
-             <tr class="text-center">
-                 <td>{{ $device->id }}</td>
-                 <td>{{ $device->code }}</td>
-                 <td>{{ $device->category->title }}</td>
-                 <td>{{ $device->trait }}</td>
-             </tr>
-
-             <tr>
-                 <th colspan="6" class="text-right" style="padding-bottom:5rem "> لوازم جانبی:
-                     {!! $device->accessories !!} </th>
-             </tr>
-             <tr>
-
-                 <th colspan="6" class="text-right">توضیحات و اظهارات درخواست کننده : {!! $device->description !!}
-                 </th>
-             </tr>
-         </tbody>
-
-         </tbody>
-         </table>
-
-     </div>
-     <div class="row" dir="rtl" style="width: 85%;">
-         <div class="col-6">
-             مهر و امضا تحویل گیرنده
          </div>
-         <div class="col-6">مهر و امضا تحویل دهنده
-         </div>
-
      </div>
      <script src="{{ asset('js/admin.js') }}"></script>
 
