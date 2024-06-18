c59a3170e9ae6a6790069f4881942fa7ae0749d2
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2024-06-18 23:55:33 +0330

    sd

--- resources/views/admin/partial/RightIconSidebar.blade.php
+++ resources/views/admin/partial/RightIconSidebar.blade.php
@@ -15,8 +15,8 @@
             </ul>
         </li>
         @endcan
-        <li><a href="javascript:void(0);" class="js-right-sidebar" title="Setting"><i
-                    class="zmdi zmdi-settings zmdi-hc-spin"></i></a></li>
+        {{-- <li><a href="javascript:void(0);" class="js-right-sidebar" title="Setting"><i
+                    class="zmdi zmdi-settings zmdi-hc-spin"></i></a></li> --}}
         <li><a aria-disabled="true" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();"
                 class="mega-menu" title="Sign Out"><i class="zmdi zmdi-power"></i></a>
             <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
